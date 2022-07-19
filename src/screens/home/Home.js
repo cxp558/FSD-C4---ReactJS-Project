54b70b6ddf5cf6f09887bf55c632238acdedfd05
cxp558 <ch.pelivanidis@gmail.com>
2022-07-19 23:25:19 +0100

    finish upcoming movies heading

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,12 +1,14 @@
-import React from "react"
-import Header from "../../common/header/Header"
+import React from "react";
+import Header from "../../common/header/Header";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div>
-            <Header />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header />
+      <div className="upcoming_header">Upcoming Movies</div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
